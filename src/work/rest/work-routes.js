a3b3af53a3bf39d9e98f73eb4900046f2eea31d3
Tender <tender121995@gmail.com>
2023-12-10 17:30:06 +0200

    task-2-improved-image-functionality

--- src/work/rest/work-routes.js
+++ src/work/rest/work-routes.js
@@ -5,6 +5,7 @@ import {
   deleteWork,
   updateWork,
   getWorkById,
+  getImage,
 } from "../service/work-service.js";
 import errorHandler from "../../middleware/error-middleware.js";
 
@@ -19,5 +20,7 @@ app.route("/delete/:id").delete(deleteWork, errorHandler);
 app.route("/update/:id").put(updateWork, errorHandler);
 
 app.route("/find/by/workId/:id").get(getWorkById, errorHandler);
+
+app.route("/getImage/:id").get(getImage, errorHandler);
 
 export default app;
